c6fb0d237fbda7bdecaf7cd3a8a837850e8eabc3
Bryan C Walsh <Walsh.C.Bryan@Gmail.com>
2019-12-09 08:51:57 -0600

    updated nerdy questions and added sound effects

--- js/bryan-questions.js
+++ js/bryan-questions.js
@@ -1,228 +1,221 @@
 const nerdy = {
     "row1": [{
-        "number" : 1,
-        "q" : "The Punisher is a fictional character appearing in comic books published by which company?",
-        "choices" : ["A: DC", "B: Mattel", "C: Marvel", "D: Dark Horse Comics"],
-        "answer" : "C",
-        "value" : 100,
-        "available" : true
+        "number": 1,
+        "q": "The Punisher is a fictional character appearing in comic books published by which company?",
+        "choices": ["A: DC", "B: Mattel", "C: Marvel", "D: Dark Horse Comics"],
+        "answer": "C",
+        "value": 100,
+        "available": true
     }, {
-        "number" : 2,
-        "q" : "SNES is the acronym for what popular gaming console released in the early 1990s?",
-        "choices" : ["A: Supreme Nintendo Extreme System", "B: SNES is not an acronym", "C: Super Nintendo Entertainment System", "D: Syndicat National l'Ensignment Secondaire"],
-        "answer" : "C",
-        "value" : 100,
-        "available" : true
+        "number": 2,
+        "q": "SNES is the acronym for what popular gaming console released in the early 1990s?",
+        "choices": ["A: Supreme Nintendo Extreme System", "B: SNES is not an acronym", "C: Super Nintendo Entertainment System", "D: Syndicat National l'Ensignment Secondaire"],
+        "answer": "C",
+        "value": 100,
+        "available": true
 
     }, {
-        "number" : 3,
-        "q" : "In the movie The Terminator, what is the name of the company that created Skynet?",
-        "choices" : ["A: Veridian Dynamics", "B: IntellArms Systems", "C: Cyber Security Solutions (CSS)", "D: Cyberdyne Systems"],
-        "answer" : "D",
-        "value" : 100,
-        "available" : true
-
-
-    }, {
-        "number" : 4,
-        "q" : "What is the secret identity of the fictional, superhero Batman?",
-        "choices" : ["A: Bruce Wayne", "B: Bruce Banner", "C: Post Malone", "D: I am Batman"],
-        "answer" : "A",
-        "value" : 100,
-        "available" : true
+        "number": 3,
+        "q": "In the movie The Terminator, what is the name of the company that created Skynet?",
+        "choices": ["A: Veridian Dynamics", "B: IntellArms Systems", "C: Cyber Security Solutions (CSS)", "D: Cyberdyne Systems"],
+        "answer": "D",
+        "value": 100,
+        "available": true
 
 
     }, {
-        "number" : 5,
-        "q" : "What is the name of the American animated sci-fi sitcom about the misadventures of a mad scientist and his grandson?",
-        "choices" : ["A: Mork & Mindy", "B: Doc & Marty", "C: Rick & Morty", "D: Batman & Robin"],
-        "answer" : "C",
-        "value" : 100,
-        "available" : true
+        "number": 4,
+        "q": "What is the secret identity of the fictional, superhero Batman?",
+        "choices": ["A: Bruce Wayne", "B: Bruce Banner", "C: Post Malone", "D: I am Batman"],
+        "answer": "A",
+        "value": 100,
+        "available": true
+
+
+    }, {
+        "number": 5,
+        "q": "What is the name of the American animated sci-fi sitcom about the misadventures of a mad scientist and his grandson?",
+        "choices": ["A: Mork & Mindy", "B: Doc & Marty", "C: Rick & Morty", "D: Batman & Robin"],
+        "answer": "C",
+        "value": 100,
+        "available": true
 
 
     }],
     "row2": [{
-        "number" : 1,
-        "q" : "In 1975 an engineer created the first electronic camera while working for what company?",
-        "choices" : ["A: Nikon ", "B: Polaroid", "C: NASA", "D: Cyberdyne Systems"],
-        "answer" : "A",
-        "value" : 200,
-        "available" : true
+        "number": 1,
+        "q": "A modulator-demodulator is a hardware device better known as what?",
+        "choices": ["A: Router ", "B: Modem ", "C: Codec", "D: Motherboard"],
+        "answer": "B",
+        "value": 500,
+        "available": true
 
 
     }, {
-        "number" : 2,
-        "q" : "'Torchwood' is an anagram and spin-off of what popular British sci-fi series?",
-        "choices" : ["A: Crowd Hoot ", "B: Cow Hotrod", "C: Doctor Who", "D: Cohort Dow"],
-        "answer" : "C",
-        "value" : 200,
-        "available" : true
+        "number": 2,
+        "q": "'Torchwood' is an anagram and spin-off of what popular British sci-fi series?",
+        "choices": ["A: Crowd Hoot ", "B: Cow Hotrod", "C: Doctor Who", "D: Cohort Dow"],
+        "answer": "C",
+        "value": 200,
+        "available": true
 
 
     }, {
-        "number" : 3,
-        "q" : "Star Trek: The Next Generation originally aired in what year?",
-        "choices" : ["A: 1987 ", "B: 1989", "C: Stardate 47457.1", "D: 1984"],
-        "answer" : "A",
-        "value" : 200,
-        "available" : true
+        "number": 3,
+        "q": "Star Trek: The Next Generation originally aired in what year?",
+        "choices": ["A: 1987 ", "B: 1989", "C: Stardate 47457.1", "D: 1984"],
+        "answer": "A",
+        "value": 200,
+        "available": true
 
 
     }, {
-        "number" : 4,
-        "q" : "What is the unit of length that is approximately 3.26 light-years?",
-        "choices" : ["A: Furlong ", "B: parseInt", "C: Lightyear", "D: Parsec"],
-        "answer" : "D",
-        "value" : 200,
-        "available" : true
+        "number": 4,
+        "q": "What is the unit of length that is approximately 3.26 light-years?",
+        "choices": ["A: Furlong ", "B: parseInt", "C: Lightyear", "D: Parsec"],
+        "answer": "D",
+        "value": 200,
+        "available": true
 
 
     }, {
-        "number" : 5,
-        "q" : "Each of a classic Rubik’s Cube six faces is covered by how many stickers?",
-        "choices" : ["A: 54 ", "B: 81 ", "C: 9 ", "D: They're stickers!?"],
-        "answer" : "C",
-        "value" : 200,
-        "available" : true
-
+        "number": 5,
+        "q": "Each of a classic Rubik’s Cube six faces is covered by how many stickers?",
+        "choices": ["A: 54 ", "B: 81 ", "C: 9 ", "D: They're stickers!?"],
+        "answer": "C",
+        "value": 200,
+        "available": true
 
     }],
     "row3": [{
-        "number" : 1,
-        "q" : "1,024 Gigabytes is equal to one what?",
-        "choices" : ["A: a A Megabyte ", "B: An Ultrabyte", "C: A Terabyte", "D: A Kilobyte"],
-        "answer" : "C",
-        "value" : 300,
-        "available" : true
 
-
-    }, {
-        "number" : 2,
-        "q" : "One kilobyte is equal to how many bytes?",
-        "choices" : ["A: 1024 ", "B: 256 ", "C: 64 ", "D: 32 "],
-        "answer" : "A",
-        "value" : 300,
-        "available" : true
-
+        "number": 1,
+        "q": "What Harvard dropout co-founded Microsoft?",
+        "choices": ["A: Marlon Brando ", "B: Bob Windows", "C: Bill Gates ", "D: Barry Doors"],
+        "answer": "C",
+        "value": 400,
+        "available": true
 
     }, {
-        "number" : 3,
-        "q" : "This planet, in our solar system, rotates clockwise...",
-        "choices" : ["A: Venus  ", "B: Neptune", "C: Earth", "D: Pluto"],
-        "answer" : "A",
-        "value" : 300,
-        "available" : true
-
-
-    }, {
-        "number" : 4,
-        "q" : "What is name of the world’s largest and most powerful particle accelerator?",
-        "choices" : ["A: Higgs Boson Cannon ", "B: Large Hadron Collider ", "C: Gamma Radiation Focus Array", "D: Sonic Centrifugal Displacement Converter "],
-        "answer" : "B",
-        "value" : 300,
-        "available" : true
-
+        "number": 2,
+        "q": "One kilobyte is equal to how many bytes?",
+        "choices": ["A: 1024 ", "B: 256 ", "C: 64 ", "D: 32 "],
+        "answer": "A",
+        "value": 300,
+        "available": true
 
     }, {
-        "number" : 5,
-        "q" : "The largest volcano ever discovered in our solar system is located on which planet?",
-        "choices" : ["A: Mars ", "B: Earth ", "C: Venus ", "D: The Sun "],
-        "answer" : "A",
-        "value" : 300,
-        "available" : true
+        "number": 3,
+        "q": "This planet, in our solar system, rotates clockwise...",
+        "choices": ["A: Venus  ", "B: Neptune", "C: Earth", "D: Pluto"],
+        "answer": "A",
+        "value": 300,
+        "available": true
 
-
+    }, {
+        "number": 4,
+        "q": "What is name of the world’s largest and most powerful particle accelerator?",
+        "choices": ["A: Higgs Boson Cannon ", "B: Large Hadron Collider ", "C: Gamma Radiation Focus Array", "D: Sonic Centrifugal Displacement Converter "],
+        "answer": "B",
+        "value": 300,
+        "available": true
+    }, {
+        "number": 5,
+        "q": "The largest volcano ever discovered in our solar system is located on which planet?",
+        "choices": ["A: Mars ", "B: Earth ", "C: Venus ", "D: The Sun "],
+        "answer": "A",
+        "value": 300,
+        "available": true
     }],
     "row4": [{
-        "number" : 1,
-        "q" : "What is the name given to an ancient analog computer that was discovered by divers off a Greek island in 1900?",
-        "choices" : ["A: Solomon's Abacus ", "B: Sony Vaio", "C: the Ikea Mechanica ", "D: Antikythera Mechanism"],
-        "answer" : "D",
-        "value" : 400,
-        "available" : true
+        "number": 1,
+        "q": "What is the name given to an ancient analog computer that was discovered by divers off a Greek island in 1900?",
+        "choices": ["A: Solomon's Abacus ", "B: Sony Vaio", "C: the Ikea Mechanica ", "D: Antikythera Mechanism"],
+        "answer": "D",
+        "value": 400,
+        "available": true
 
 
     }, {
-        "number" : 2,
-        "q" : "What Harvard dropout co-founded Microsoft?",
-        "choices" : ["A: Marlon Branddo ", "B: Bob Windows", "C: Bill Gates ", "D: Barry Doors"],
-        "answer" : "C",
-        "value" : 400,
-        "available" : true
+        "number": 2,
+        "q": "1,024 Gigabytes is equal to one what?",
+        "choices": ["A: a A Megabyte ", "B: An Ultrabyte", "C: A Terabyte", "D: A Kilobyte"],
+        "answer": "C",
+        "value": 300,
+        "available": true
 
 
     }, {
-        "number" : 3,
-        "q" : "What is the most abundant element in the earth’s atmosphere?",
-        "choices" : ["A: Carbon ", "B: Nitrogen", "C: Carbon Dioxide", "D: Oxygen"],
-        "answer" : "B",
-        "value" : 400,
-        "available" : true
+        "number": 3,
+        "q": "What is the most abundant element in the earth’s atmosphere?",
+        "choices": ["A: Carbon ", "B: Nitrogen", "C: Carbon Dioxide", "D: Oxygen"],
+        "answer": "B",
+        "value": 400,
+        "available": true
 
 
     }, {
-        "number" : 4,
-        "q" : "What was the name of the first electronic general-purpose computer?",
-        "choices" : ["A: ENIAC ", "B: Ernie ", "C: HP-dewlett", "D: ONARD"],
-        "answer" : "A",
-        "value" : 400,
-        "available" : true
+        "number": 4,
+        "q": "What was the name of the first electronic general-purpose computer?",
+        "choices": ["A: ENIAC ", "B: Ernie ", "C: HP-dewlett", "D: ONARD"],
+        "answer": "A",
+        "value": 400,
+        "available": true
 
 
     }, {
-        "number" : 5,
-        "q" : "Bruce Willis played a convict turned time traveler in what 1995 movie?",
-        "choices" : ["A: Monkey Bone ", "B: A Dozen Monkeys ", "C: 12 Monkeys", "D: Lock, Stock, and Two Smoking Monkeys"],
-        "answer" : "C",
-        "value" : 400,
-        "available" : true
+        "number": 5,
+        "q": "Bruce Willis played a convict turned time traveler in what 1995 movie?",
+        "choices": ["A: Monkey Bone ", "B: A Dozen Monkeys ", "C: 12 Monkeys", "D: Lock, Stock, and Two Smoking Monkeys"],
+        "answer": "C",
+        "value": 400,
+        "available": true
 
 
     }],
     "row5": [{
-        "number" : 1,
-        "q" : "What does HTTP stand for in a website address?",
-        "choices" : ["A: Hip To Try Pistachios ", "B: HyperText Transition Procedure", "C: HyperText Transfer Protocol ", "D: HyperText Terminal Process"],
-        "answer" : "C",
-        "value" : 500,
-        "available" : true
+        "number": 1,
+        "q": "What does HTTP stand for in a website address?",
+        "choices": ["A: Hip To Try Pistachios ", "B: HyperText Transition Procedure", "C: HyperText Transfer Protocol ", "D: HyperText Terminal Process"],
+        "answer": "C",
+        "value": 500,
+        "available": true
 
 
     }, {
-        "number" : 2,
-        "q" : "A modulator-demodulator is a hardware device better known as what?",
-        "choices" : ["A: Router ", "B: Modem ", "C: Codec", "D: Motherboard"],
-        "answer" : "B",
-        "value" : 500,
-        "available" : true
+        "number": 2,
+        "q": "In 1975 an engineer created the first electronic camera while working for what company?",
+        "choices": ["A: Nikon ", "B: Polaroid", "C: NASA", "D: Cyberdyne Systems"],
+        "answer": "A",
+        "value": 200,
+        "available": true
 
 
     }, {
-        "number" : 3,
-        "q" : "The malleus, incus and stapes are found in what part of the human body?",
-        "choices" : ["A: Hand ", "B: Foot ", "C: Ear ", "D: Nose "],
-        "answer" : "C",
-        "value" : 500,
-        "available" : true
+        "number": 3,
+        "q": "The malleus, incus and stapes are found in what part of the human body?",
+        "choices": ["A: Hand ", "B: Foot ", "C: Ear ", "D: Nose "],
+        "answer": "C",
+        "value": 500,
+        "available": true
 
 
     }, {
-        "number" : 4,
-        "q" : "Ganymede, the largest moon in our solar system, belongs to which planet?",
-        "choices" : ["A: Jupiter ", "B: Uranus ", "C: Neptune ", "D: Saturn"],
-        "answer" : "A",
-        "value" : 500,
-        "available" : true
+        "number": 4,
+        "q": "Ganymede, the largest moon in our solar system, belongs to which planet?",
+        "choices": ["A: Jupiter ", "B: Uranus ", "C: Neptune ", "D: Saturn"],
+        "answer": "A",
+        "value": 500,
+        "available": true
 
 
     }, {
-        "number" : 5,
-        "q" : "Orson Welles provided the voice for which Transformer in “The Transformers: The Movie” released in 1986?",
-        "choices" : ["A: Optimus Prime ", "B: Unicron ", "C: Megatron", "D: Ironhide "],
-        "answer" : "B",
-        "value" : 500,
-        "available" : true
+        "number": 5,
+        "q": "Orson Welles provided the voice for which Transformer in “The Transformers: The Movie” released in 1986?",
+        "choices": ["A: Optimus Prime ", "B: Unicron ", "C: Megatron", "D: Ironhide "],
+        "answer": "B",
+        "value": 500,
+        "available": true
 
 
     }]
